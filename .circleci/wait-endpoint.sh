a4e27b55a9c6a7d76b00b9c75f927efefcb8dce1
bott <bott@adobe.com>
2023-09-19 10:13:36 +0200

    wait until wknd page is there

--- .circleci/wait-endpoint.sh
+++ .circleci/wait-endpoint.sh
@@ -1,24 +1,22 @@
 #!/usr/bin/env bash
 ##############################################################################################
-# Wait for URLs until return HTTP 200
+# Wait for a url to contain a substring in the title attribute
 #
-# - Just pass as many urls as required to the script - the script will wait for each, one by one
-#
-# Example: ./wait_for_urls.sh "${MY_VARIABLE}" "http://192.168.56.101:8080"
+# Example: ./wait-endpoint -u "https://google.com" -t "google" -s 10 -o 300
 ##############################################################################################
-# Original author: https://gist.github.com/eisenreich/195ab1f05715ec86e300f75d007d711c
-# modified with a timeout of 300s seconds and a retry of ever 10
+# based on the work of: https://gist.github.com/eisenreich/195ab1f05715ec86e300f75d007d711c
 
-TIMEOUT=300
-SLEEP=15
 
-wait-for-url() {
-    echo "Testing: $1"
-    # shellcheck disable=SC2016
-    timeout --foreground -s TERM "${2}s" bash -c \
-        'while [[ "$(curl -s -o /dev/null -m 3 -L -w ''%{http_code}'' ${0})" != "200" ]];\
-        do echo "Waiting for ${0}" && sleep ${1};\
-        done' "${1}" "${3}"
+wait_for_page() {
+    echo "Testing: \"$1\" for title \"$2\" with a timeout of $3s"
+
+    r=$(curl -s -L "${1}" | grep \<title\> | xargs | sed s/"<title>"// | sed s/"<\/title>"//)
+    echo "$r"
+
+    timeout --foreground -s TERM "${3}s" bash -c \
+        'while [[ "$(curl -s -L "${0}" | grep \<title\> | xargs | sed s/"<title>"// | sed s/"<\/title>"//)" != *"${1}"* ]];\
+        do echo "Waiting ${0} for title ${1} " && sleep ${2};\
+        done' "${1}" "${2}" "${4}"
 
     local TIMEOUT_RETURN="$?"
     echo $TIMEOUT_RETURN
@@ -32,14 +30,47 @@ wait-for-url() {
         echo "Other error with code ${TIMEOUT_RETURN}: ${1} -> EXIT"
         exit "${TIMEOUT_RETURN}"
     fi
+
 }
 
-echo "Wait for URLs: $*"
-
-for URL in "$@"; do
-    echo ""
-    wait-for-url "${URL}" "${TIMEOUT}" "${SLEEP}"
+# Read input parameters
+while getopts u:t:s:o:   option
+do
+  case "${option}"
+  in
+    u) URL=${OPTARG};;
+    t) TITLE=${OPTARG};;
+    s) SLEEP=${OPTARG};;
+    o) TIMEOUT=${OPTARG};;
+    *) echo "usage: $0 -u <URL> -t <PAGE TITLE>" >&2
+       exit 1 ;;
+  esac
 done
+shift $((OPTIND -1))
+
+if [ -z "$URL" ]
+then
+    echo "URL empty, exiting"
+    exit 1
+fi
+
+if [ -z "$TITLE" ]
+then
+    echo "page title empty, exiting"
+    exit 1
+fi
+
+if [ -z "$SLEEP" ]
+then
+  SLEEP=15
+fi
+
+if [ -z "$TIMEOUT" ]
+then
+  TIMEOUT=300
+fi
+
+wait_for_page "${URL}" "${TITLE}" "${TIMEOUT}" "${SLEEP}"
 
 echo ""
 echo "SUCCESSFUL"
